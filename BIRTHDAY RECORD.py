b4e9230116ba13d2735ce35f8bd269b616fdc7dd
Udokaku Ugochukwu <63649352+UdokaVrede@users.noreply.github.com>
2020-07-26 13:13:05 +0100

    TASK
    This program would check names inputted by the user against already existing data and where it doesn't exist the user is given a choice to add the new data to the record.
    
    This is my first experience with updating dictionaries all appreciation to "AUTOMATING THE BORING STUFF WITH PYTHON"

---
+++ BIRTHDAY RECORD.py
@@ -0,0 +1,29 @@
+birthdays={'Mum': 'Aug 15th',
+           'Butter boy' : 'July 10th',
+           'Bambi' : 'Apr 11th'}
+           
+while True:
+    name=input('Enter a name or leave blank to quit: ')
+    if name == '':
+        print('The birthdays in the record are:')
+        print(birthdays)
+        
+        break
+    
+    if name in birthdays:
+        print(name,'is born on ' + birthdays[name])
+    else:
+        print('Sorry the name '+ name +' is not valid','\n')
+        
+        print('do you wish to update the record?')
+        user=input('press yes or no to quit: ')
+        if user == 'yes':
+            birthdate=input('Enter the birthdate: ')
+            birthdays[name]=birthdate
+            print('The record has been updated','\n')
+            print('Thank you for using this, you may continue or blank to quit')
+            
+        else:
+            print('Thank you for using this, You may wish to continue or quit')
+        
+        
